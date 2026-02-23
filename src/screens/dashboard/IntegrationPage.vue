e12e9733e4924db4e1c66c8e3fa4168a2b4000cb
Abhinash Sharma <52877872+Abhinash16@users.noreply.github.com>
2026-02-23 18:19:23 +0530

    feat: Add 'gpt-4o-mini' model to chatGpt models list

--- src/screens/dashboard/IntegrationPage.vue
+++ src/screens/dashboard/IntegrationPage.vue
@@ -251,6 +251,7 @@ export default {
         "gpt-3.5-turbo",
         "gpt-4",
         "gpt-4-turbo",
+        "gpt-4o-mini",
       ],
       chatgptModel: "gpt-3.5-turbo",
       chatgptContentPrompt:
